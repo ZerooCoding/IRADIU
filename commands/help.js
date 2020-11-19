c6bd1814199953cb48a77d906bebb151f8019fa1
Shadmehr <56414145+shade-m1@users.noreply.github.com>
2020-11-19 11:51:47 +0330

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -4,4 +4,17 @@ module.exports = {
   name: "help",
   aliases: ["h"],
   description: "Just Do iu!play and Enjoy ",
-}
+  execute(message) {
+    let commands = message.client.commands.array();
+
+    let helpEmbed = new MessageEmbed()
+      .setTitle(`${message.client.user.username} Help`)
+      .setDescription("List of all commands")
+      .setColor("#F8AA2A");
+
+    helpEmbed.setTimestamp();
+
+    return message.channel.send(helpEmbed).catch(console.error);
+  }
+};
+
